42163953fd91e2d41798300dbf72d0ab4da11c3f
Moss McLaughlin <mosslaugh@yahoo.com>
2017-06-04 18:04:43 -0600

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -14,24 +14,7 @@ class CNN(object):
         # Keeping track of l2 regularization loss (optional)
         l2_loss = tf.constant(0.0)
 
-        ''' 
-        # Embedding layer
-        with tf.device('/cpu:0'), tf.name_scope("embedding"):
-            
-            if embedding: None
-             
-                #load_embedding()
-            else:
-                self.W = tf.Variable(
-                    tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
-                name="W")
-            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x[0])
-            self.embedded_chars2 = tf.nn.embedding_lookup(self.W, self.input_x[1])
-            self.X_data = [[ 
-                for line in input_x:
-                    for                  
-#           self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
-        '''        
+        
         self.embedded_chars = self.input_x
         # Create a convolution + maxpool layer for each filter size
         pooled_outputs = []
@@ -42,7 +25,6 @@ class CNN(object):
                 W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
                 b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
                 conv = tf.nn.conv2d(
-#                    self.embedded_chars_expanded,
                     self.embedded_chars,
                     W,
                     strides=[1, 1, 1, 1],
